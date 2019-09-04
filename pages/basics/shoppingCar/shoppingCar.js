521aea3e36230eda663becd51e65f8b66a2b2e39
enutpeN <haibaichi1001@qq.com>
2019-09-04 18:04:51 +0800

    购物车场景

--- pages/basics/shoppingCar/shoppingCar.js
+++ pages/basics/shoppingCar/shoppingCar.js
@@ -1,15 +1,11 @@
-// pages/basics/scene/scene.js
+// pages/component/shoppingCar/shoppingCar.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    scenePlan:[
-      { imgsrc:"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3766596481,2274311292&fm=26&gp=0.jpg",text:'购物车'},
-      { imgsrc: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3597093101,1937951774&fm=26&gp=0.jpg", text: '购物车'},
-      { imgsrc: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2435077873,909358147&fm=26&gp=0.jpg", text: '购物车' },
-    ]
+
   },
 
   /**
