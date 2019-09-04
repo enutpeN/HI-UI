0ad5c434111a34f7250ec47270140b947d2a7c93
unknown <unknown@example.com>
2019-09-05 00:16:22 +0800

    购物车9.5日补

--- pages/basics/shoppingCar/shoppingCar.js
+++ pages/basics/shoppingCar/shoppingCar.js
@@ -5,7 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list:[{
+      id:1,
+      title: 'ameirg香辣鸡翅汉堡 德国配方来自欧洲的味道',
+      image: 'http://img4.imgtn.bdimg.com/it/u=1322201793,35923405&fm=26&gp=0.jpg',
+      pro_name: "600g",
+      num: 1,
+      price: 180,
+      selected: true
+    }]
   },
 
   /**
@@ -22,33 +30,50 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //递加计算
+  add(e){
+    //点击获取索引
+    const index = e.currentTarget.dataset.index;
+    //获取商品列表
+    let list=this.data.list
+    //获取商品此时数量
+    let num = list[index].num
+    //点击递增
+    num=num+1
+    list[index].num=num
+    //重新渲染数组
+    this.setData({list:list})
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  //递减计算
+  minus(e){
+    //点击获取索引
+    const index = e.currentTarget.dataset.index;
+    //获取商品列表
+    let list = this.data.list
+    //获取商品此时数量
+    let num = list[index].num
+    //小于等于1 不能再减少了
+    if(num<=1){
+      wx.showToast({
+        title: '宝贝不能再少啦，受不了啦',
+        icon: 'none',
+      })
+      return
+    }
+    //点击递减少
+    num = num - 1
+    list[index].num = num
+    //重新渲染数组
+    this.setData({ list: list })
   },
+  
+ 
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
 
-  },
 
   /**
    * 页面上拉触底事件的处理函数
