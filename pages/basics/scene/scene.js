8be84784bf708ab142e23bb6147fead88b3f4fe6
unknown <unknown@example.com>
2020-02-16 18:43:07 +0800

    2.16
    细节修改

--- pages/basics/scene/scene.js
+++ pages/basics/scene/scene.js
@@ -6,9 +6,9 @@ Page({
    */
   data: {
     scenePlan:[
-      { imgsrc:"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3766596481,2274311292&fm=26&gp=0.jpg",text:'购物车'},
-      { imgsrc: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3597093101,1937951774&fm=26&gp=0.jpg", text: '美团点餐'},
-      { imgsrc: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2435077873,909358147&fm=26&gp=0.jpg", text: '通讯录' },
+      { imgsrc:"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3766596481,2274311292&fm=26&gp=0.jpg",text:'模拟购物车'},
+      { imgsrc: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3597093101,1937951774&fm=26&gp=0.jpg", text: '模拟美团点餐'},
+      { imgsrc: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2435077873,909358147&fm=26&gp=0.jpg", text: '模拟通讯录' },
     ]
   },
 
